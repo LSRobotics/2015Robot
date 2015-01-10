02cba324a8f3d12c93a51e1d07e4835e8a401f56
msbense <markbense2016@lschs.org>
2015-01-10 11:39:10 -0500

    added int variable

--- 2015_Robot/src/org/usfirst/frc/team5181/robot/Robot.java
+++ 2015_Robot/src/org/usfirst/frc/team5181/robot/Robot.java
@@ -19,14 +19,14 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-
+    	
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	int a = 5;
     }
 
     /**
