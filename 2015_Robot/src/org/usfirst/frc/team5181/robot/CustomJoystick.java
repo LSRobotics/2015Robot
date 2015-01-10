5870e4840d24cfaa5484e5e12f212f93375a0da3
LSRobotics <LSRobotics@192.168.121.195>
2015-01-10 14:46:44 -0500

    robot now can tankDrive in testPeriodic

--- 2015_Robot/src/org/usfirst/frc/team5181/robot/CustomJoystick.java
+++ 2015_Robot/src/org/usfirst/frc/team5181/robot/CustomJoystick.java
@@ -2,6 +2,24 @@ package org.usfirst.frc.team5181.robot;
 
 import edu.wpi.edu.wpi.first
 
-public class CustomJoystick extends JoyStick{
-	Joystick
+public class CustomJoystick extends JoyStick implements StaticVars{
+	//Gamepad
+	public static int A_Button = 1;
+	public static int B_Button = 2;
+	public static int X_Button = 3;
+	public static int Y_Button = 4;
+	public static int LEFT_Bumper = 5;
+	public static int RIGHT_Bumper = 6;
+	public static int BACK = 7;
+	public static int START = 8;
+	public static int LEFT_Stick_DOWN = 9;
+	public static int RIGHT_Stick_DOWN = 10;	
+	//Axis
+	public static int LEFT_Stick_X = 0;
+	public static int LEFT_Stick_Y = 1;
+	public static int LEFT_Trigger = 2;
+	public static int RIGHT_Trigger = 3;
+	public static int RIGHT_Stick_X = 4;
+	public static int RIGHT_Stick_Y = 5;
+	
 }
--- 2015_Robot/src/org/usfirst/frc/team5181/robot/StaticVars.java
+++ 2015_Robot/src/org/usfirst/frc/team5181/robot/StaticVars.java
@@ -8,25 +8,7 @@ public interface StaticVars {
 	public static int FRPort = 3;
 	public static int RRPort = 4;
 	
-	//Gamepad
-	public static int A_Button = 1;
-	public static int B_Button = 2;
-	public static int X_Button = 3;
-	public static int Y_Button = 4;
-	public static int LEFT_Bumper = 5;
-	public static int RIGHT_Bumper = 6;
-	public static int BACK = 7;
-	public static int START = 8;
-	public static int LEFT_Stick_DOWN = 9;
-	public static int RIGHT_Stick_DOWN = 10;
-	
-	//Axis
-	public static int LEFT_Stick_X = 0;
-	public static int LEFT_Stick_Y = 1;
-	public static int LEFT_Trigger = 2;
-	public static int RIGHT_Trigger = 3;
-	public static int RIGHT_Stick_X = 4;
-	public static int RIGHT_Stick_Y = 5;
+	public static int JOYSTICK_PORT = 1;
 
 	
 }
